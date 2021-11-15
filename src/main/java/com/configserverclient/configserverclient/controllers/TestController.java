d358089d795a55c1fca011ba5a1e7436b091d0be
Rafa Dominguez <rafa.realmadrid.rdm@gmail.com>
2021-11-15 14:25:48 +0100

    Implementacion profiles

--- src/main/java/com/configserverclient/configserverclient/controllers/TestController.java
+++ src/main/java/com/configserverclient/configserverclient/controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.configserverclient.configserverclient.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ public class TestController {
 	@Value("${some.value}")
 	private String myValue;
 	
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+	
 	@GetMapping(path = "/myValue")
 	public String myValue() {
-		return this.myValue;
+		return this.myValue + " desde el perfil: " + activeProfile;
 	}
 }
